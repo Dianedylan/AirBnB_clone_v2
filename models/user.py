03d7298763bed763fbfb254653826bd61065df02
Dianedylan <dianagatwiriikiamba@gmail.com>
2022-09-14 21:51:48 +0300

    change user.py

--- models/user.py
+++ models/user.py
@@ -1,25 +1,23 @@
 #!/usr/bin/python3
-"""This is the user class"""
+"""This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-from os import getenv
 
 
 class User(BaseModel, Base):
-    """This is the class for user
-    Attributes:
-        __tablename__ : users table
-        email: email address
-        password: password for your login
-        first_name: first name
-        last_name: last name
-    """
-    __tablename__ = "users"
-    email = Column("email", String(128), nullable=False)
-    password = Column("password", String(128), nullable=False)
-    first_name = Column("first_name", String(128), nullable=True)
-    last_name = Column("last_name", String(128), nullable=True)
-    places = relationship('Place', backref='user')
-    reviews = relationship('Review', backref='user')
+    """This class defines a user by various attributes"""
+    __tablename__ = 'users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
+    places = relationship('Place', backref='user', cascade='all, delete')
+    reviews = relationship('Review', backref='user', cascade='all, delete')
+
+    def __init__(self, *args, **kwargs):
+        self.email = kwargs['email']
+        self.password = kwargs['password']
+        self.first_name = kwargs['first_name']
+        self.last_name = kwargs['last_name']
+        super().__init__(*args)
